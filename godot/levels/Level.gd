dbacaf04f58f5dff2b010e499b5b034644ae9a81
Tch1b0 <Jpour3006@gmail.com>
2022-07-10 11:32:45 +0200

    remove groups

--- godot/levels/Level.gd
+++ godot/levels/Level.gd
@@ -5,5 +5,11 @@ func _ready():
 	get_tree().call_group("characters", "connect", "collided", self, "characterCollision")
 	
 func characterCollision(object, character):	
-	if character.is_in_group("enemy") and object.is_in_group("player"):
-		character.attack()
+	if (
+		character is Enemy and
+		(
+			object is Player
+		)
+	):
+		if character is Enemy:
+			character.attack()
