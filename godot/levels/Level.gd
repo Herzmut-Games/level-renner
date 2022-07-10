d4c68782ae9d334a647a1191b7ea9b26318ed410
Tch1b0 <Jpour3006@gmail.com>
2022-07-10 08:39:54 +0200

    enhance dash animation

--- godot/levels/Level.gd
+++ godot/levels/Level.gd
@@ -5,11 +5,5 @@ func _ready():
 	get_tree().call_group("characters", "connect", "collided", self, "characterCollision")
 	
 func characterCollision(object, character):	
-	if (
-		character is Enemy and
-		(
-			object is Player
-		)
-	):
-		if character is Enemy:
-			character.attack()
+	if character.is_in_group("enemy") and object.is_in_group("player"):
+		character.attack()
