5784adb7a3b64dbee96933ae35ab20fb16b78b6a
Raphael Pour <info@raphaelpour.de>
2022-07-11 20:27:59 +0200

    jump/walljump: use sounds

--- godot/characters/player/skills/Walljump.gd
+++ godot/characters/player/skills/Walljump.gd
@@ -4,8 +4,18 @@ class_name Walljump
 export var bounce = 180
 export var jump_speed = 200
 
+onready var sounds = [
+	preload("res://assets/sound/Jump_wall2.wav"),
+	preload("res://assets/sound/Jump_wall3.wav"),
+	preload("res://assets/sound/Jump_wall4.wav"),
+	preload("res://assets/sound/Jump_wall5.wav"),
+	preload("res://assets/sound/Jump_wall6.wav")
+]
+
 func use(character: Character):
 	if character.is_on_wall() and $SkillCooldown.available(): #wall jump
+		$AudioStreamPlayer.stream = sounds[clamp($SkillCooldown.current_uses-1, 0, sounds.size() -1)]
+		$AudioStreamPlayer.play()
 		character.velocity = Vector2((-1 if character.velocity.x > 0 else 1) * bounce, 0) + character.gravity_dir() * -jump_speed / 1.25
 		$SkillCooldown.use()
 		return true
