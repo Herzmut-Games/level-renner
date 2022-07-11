bd8ee6721e71dcf5971f07f69aa983cb23bf4e9a
Adrian Huber <adihuber12@gmail.com>
2022-07-11 19:41:33 +0200

    Fix Skills

--- godot/characters/player/skills/Dash.gd
+++ godot/characters/player/skills/Dash.gd
@@ -3,20 +3,14 @@ class_name Dash
 
 export var dash_speed = 5000
 
-var cooldown: SkillCooldown
-
-func _ready():
-	cooldown = SkillCooldown.new()
-	add_child(cooldown)
-
 func use(character: Character):
-	if cooldown.available():
+	if $SkillCooldown.available():
 		var direction = character.get_movement_direction()
 		if direction == 0.0:
 			direction = character.velocity.x
 		
 		character.velocity.x += (1 if direction >= 0 else -1) * dash_speed
-		cooldown.use()
+		$SkillCooldown.use()
 		
 		return true
 	return false
--- godot/characters/player/skills/Walljump.gd
+++ godot/characters/player/skills/Walljump.gd
@@ -4,17 +4,9 @@ class_name Walljump
 export var bounce = 180
 export var jump_speed = 200
 
-var cooldown: SkillCooldown
-
-func _ready():
-	cooldown = SkillCooldown.new()
-	add_child(cooldown)
-
 func use(character: Character):
-	print(get_children())
-	
-	if character.is_on_wall() and cooldown.available(): #wall jump
+	if character.is_on_wall() and $SkillCooldown.available(): #wall jump
 		character.velocity = Vector2((-1 if character.velocity.x > 0 else 1) * bounce, 0) + character.gravity_dir() * -jump_speed / 1.25
-		cooldown.use()
+		$SkillCooldown.use()
 		return true
 	return false
