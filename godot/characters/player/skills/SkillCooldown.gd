cb396b66a23a5819c6b654326363dacd639e2c42
Raphael Pour <info@raphaelpour.de>
2022-07-11 20:28:18 +0200

    skill_cooldown: refactor use counter

--- godot/characters/player/skills/SkillCooldown.gd
+++ godot/characters/player/skills/SkillCooldown.gd
@@ -2,7 +2,7 @@ extends Timer
 class_name SkillCooldown
 
 export var max_uses = 5;
-var current_uses = max_uses;
+var current_uses = 0;
 
 func _ready():
 	self.connect("timeout", self, "_timeout")
@@ -10,11 +10,10 @@ func _ready():
 func use():
 	if current_uses == max_uses:
 		self.start()
-	current_uses -= 1
-	
+	current_uses +=1
+
 func available():
-	return current_uses > 0
+	return current_uses <= max_uses
 	
 func _timeout():
-	if current_uses < max_uses:
-		current_uses += 1
+	current_uses = min(0, current_uses-1)
