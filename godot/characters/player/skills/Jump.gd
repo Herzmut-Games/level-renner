b94c402186f1c736cb91860423a700eb19e0da86
Marvin Altemeier <marvin.altemeier@hey.com>
2022-07-12 22:28:53 +0200

    tweak jumping

--- godot/characters/player/skills/Jump.gd
+++ godot/characters/player/skills/Jump.gd
@@ -19,7 +19,10 @@ func _unhandled_input(event):
 func _physics_process(delta):
 	jump_muliplier -= decay * delta
 	$RayCastLeft.cast_to.y = abs($RayCastLeft.cast_to.y) * character.gravity_dir().y
+	$RayCastLeft.position.y = abs($RayCastLeft.position.y) * character.gravity_dir().y
+
 	$RayCastRight.cast_to.y = abs($RayCastRight.cast_to.y) * character.gravity_dir().y
+	$RayCastRight.position.y = abs($RayCastRight.position.y) * character.gravity_dir().y
 
 	if Input.is_action_pressed("jump") and jump_muliplier >= 0 and character.state != character.States.FALL:
 		character.velocity.y += jump_muliplier * -character.gravity_dir().y
