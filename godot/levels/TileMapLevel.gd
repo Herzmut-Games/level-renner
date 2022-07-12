46605bb6afd07745e788b4c96d4b005a8fe4bffb
Marvin Altemeier <marvin.altemeier@hey.com>
2022-07-12 19:37:55 +0200

    fix tilemap script

--- godot/levels/TileMapLevel.gd
+++ godot/levels/TileMapLevel.gd
@@ -6,7 +6,7 @@ export var mirror = true
 func set_cell(x: int, y: int, tile: int, flip_x: bool = false, flip_y: bool = false, transpose: bool = false, autotile_coord: Vector2 = Vector2()) -> void:
 	.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
 
-	if mirror and autotile_coord.y < 4:
+	if mirror:
 		.set_cell(x, (y + 1)* -1, tile, flip_x, flip_y, transpose, autotile_coord + Vector2(4, 0))
 
 	pass
