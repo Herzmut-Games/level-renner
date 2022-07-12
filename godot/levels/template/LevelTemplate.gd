ae3d0023860655427478bbf31a6ac7aa81a55213
Marvin Altemeier <marvin.altemeier@hey.com>
2022-07-12 20:26:44 +0200

    Create base level

---
+++ godot/levels/template/LevelTemplate.gd
@@ -0,0 +1,19 @@
+extends Node2D
+class_name LevelTemplate
+
+func _ready():
+	get_tree().call_group("characters", "connect", "collided", self, "character_collision")
+	get_tree().call_group("traps", "connect", "collided", self, "trap_collision")
+
+	GlobalGame.init($Player/Camera2D, $Player)
+
+func trap_collision(character, object):
+	character_collision(object, character)
+
+func character_collision(object, character):
+	if character is Player:
+		if object is Enemy:
+			object.attack(character)
+			character.hit()
+		if object.is_in_group("traps"):
+			character.hit()
