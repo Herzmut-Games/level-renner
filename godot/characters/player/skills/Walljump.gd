6208927bc1098c4bb15e7fb2b731379f27220b93
Adrian Huber <adihuber12@gmail.com>
2022-07-12 22:32:30 +0200

    Fix no walljump areas

--- godot/characters/player/skills/Walljump.gd
+++ godot/characters/player/skills/Walljump.gd
@@ -14,11 +14,21 @@ onready var sounds = [
 
 func _unhandled_input(event):
 	if event.is_action_pressed("jump", false, false):
-		if $RayCastLeft.is_colliding() or $RayCastRight.is_colliding():
-			$AudioStreamPlayer.stream = sounds[clamp($SkillCooldown.current_uses-1, 0, sounds.size() -1)]
-			$AudioStreamPlayer.play()
-			
-			character.velocity = Vector2((-1 if $RayCastRight.is_colliding() else 1) * bounce, 0) + character.gravity_dir() * -jump_speed / 1.25
+		var colliderLeft = $RayCastLeft.get_collider()
+		var colliderRight =  $RayCastRight.get_collider()
 
-			$SkillCooldown.use()
-			get_tree().set_input_as_handled()
+		var collider = null
+		if not(colliderLeft == null or colliderLeft.get_collision_layer() & 0x4):
+			collider = colliderLeft
+		elif not(colliderRight == null or colliderRight.get_collision_layer() & 0x4):
+			collider = colliderRight
+		else:
+			return
+
+		get_tree().set_input_as_handled()
+		$AudioStreamPlayer.stream = sounds[clamp($SkillCooldown.current_uses-1, 0, sounds.size() -1)]
+		$AudioStreamPlayer.play()
+
+		character.velocity = Vector2((-1 if collider == colliderRight else 1) * bounce, 0) + character.gravity_dir() * -jump_speed / 1.25
+
+		$SkillCooldown.use()
