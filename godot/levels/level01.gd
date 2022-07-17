37c9d33c451e6f305e124966899ec32bbe27ae93
Flipez <code@brauser.io>
2022-07-17 21:34:25 +0200

    fix player position and startup text

--- godot/levels/level01.gd
+++ godot/levels/level01.gd
@@ -6,16 +6,7 @@ func _ready():
 	._ready()
 	$BackgroundMusic.stream = loop
 	$BackgroundMusic.play()
-	get_tree().paused = true
 	$UI/Messages/TopLeft.text = "Level 1"
-	$UI/Messages/Center.text = "3"
-	yield(get_tree().create_timer(1.0), "timeout")
-	$UI/Messages/Center.text = "2"
-	yield(get_tree().create_timer(1.0), "timeout")
-	$UI/Messages/Center.text = "1"
-	yield(get_tree().create_timer(1.0), "timeout")
-	$UI/Messages/Center.text = ""
-	get_tree().paused = false
 
 func _on_LevelEnd_body_entered(body):
 	GlobalGame.stop_run()
