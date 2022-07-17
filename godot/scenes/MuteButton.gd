81970315b6c709a8b9d7c03c82eb7a5f42d1ca8f
Tch1b0 <Jpour3006@gmail.com>
2022-07-17 14:46:05 +0200

    fix mutebutton appearance

--- godot/scenes/MuteButton.gd
+++ godot/scenes/MuteButton.gd
@@ -1,5 +1,9 @@
 extends TextureButton
 
+func _ready():
+	var bus = AudioServer.get_bus_index("Master")
+	pressed = AudioServer.is_bus_mute(bus)
+
 func _on_MuteButton_pressed():
 	var bus = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_mute(bus, !AudioServer.is_bus_mute(bus))
